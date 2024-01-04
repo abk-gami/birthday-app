b32a65894f38cee56436f673302cecb34815fd9c
Abk <127848306+abk-gami@users.noreply.github.com>
2024-01-04 22:03:05 +0100

    Update Birthday.js

--- Birthday.js
+++ Birthday.js
@@ -29,7 +29,7 @@ const CelebrateBirthday = () => {
       return (
         <View style={styles.birthdayCard}>
           <Text style={styles.cardText}>Happy Birthday {name}!</Text>
-          <Text style={styles.cardText}>You are n {age} years old.</Text>
+          <Text style={styles.cardText}>You are now {age} years old.</Text>
           <Text style={styles.cardText}>
             Born on {month} {date}, {year}.
           </Text>
